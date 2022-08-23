d211f47242ddf4cc9c55f6e92cca507bdd1588c0
Marius Tache <marius.tache@nxp.com>
2022-08-23 09:05:53 -0700

    [MATTER-379][K32W0] Add patch files
    * Added patch files until this PR is merged and
    a new SDK is released:
    https://bitbucket.sw.nxp.com/projects/CONNINT/repos/connectivity-framework/pull-requests/2906/overview
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>

--- third_party/nxp/k32w0_sdk/sdk_fixes/patch_k32w_sdk.sh
+++ third_party/nxp/k32w0_sdk/sdk_fixes/patch_k32w_sdk.sh
@@ -16,10 +16,17 @@ SOURCE=${BASH_SOURCE[0]}
 SOURCE_DIR=$(cd "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)
 
 convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Source/fsl_os_abstraction_free_rtos.c
+convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Source/fsl_os_abstraction_bm.c
+convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Interface/fsl_os_abstraction.h
+convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/SecLib/SecLib.c
+convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/SecLib/SecLib.h
 convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OtaSupport/Interface/OtaUtils.h
+convert_to_dos "$NXP_K32W0_SDK_ROOT"/middleware/mbedtls/port/ksdk/sha256_alt.h
 
-patch -N --binary -d "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/Source/ -p1 <"$SOURCE_DIR/fsl_os_abstraction_free_rtos_c.patch"
+patch -N --binary -d "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OSAbstraction/ -p1 <"$SOURCE_DIR/fsl_os_abstraction.patch"
+patch -N --binary -d "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/SecLib/ -p1 <"$SOURCE_DIR/SecLib.patch"
 patch -N --binary -d "$NXP_K32W0_SDK_ROOT"/middleware/wireless/framework/OtaSupport/Interface/ -p1 <"$SOURCE_DIR/OtaUtils_h.patch"
+patch -N --binary -d "$NXP_K32W0_SDK_ROOT"/middleware/mbedtls/port/ksdk/ -p1 <"$SOURCE_DIR/sha256_alt.patch"
 
-echo "K32W SDK 2.6.6 was patched!"
+echo "K32W SDK 2.6.7 was patched!"
 exit 0
