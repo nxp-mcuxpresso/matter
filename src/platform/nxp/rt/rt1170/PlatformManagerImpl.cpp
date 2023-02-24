6b379e1602a11c65c829157cc52fdc95ed36a327
Gatien Chapon <gatien.chapon@nxp.com>
2023-02-24 16:06:29 +0100

    [RT1170] Removing unnecessary call when calling platform Shutdown function
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- src/platform/nxp/rt/rt1170/PlatformManagerImpl.cpp
+++ src/platform/nxp/rt/rt1170/PlatformManagerImpl.cpp
@@ -509,7 +509,6 @@ void PlatformManagerImpl::_Shutdown()
 
     /* Handle the server shutting down & emit the ShutDown event*/
     PlatformMgr().HandleServerShuttingDown();
-    PlatformMgr().StopEventLoopTask();
     /* Shutdown all layers */
     Internal::GenericPlatformManagerImpl_FreeRTOS<PlatformManagerImpl>::_Shutdown();
 }
