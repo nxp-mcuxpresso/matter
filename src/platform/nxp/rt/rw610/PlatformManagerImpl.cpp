f04cff3cad1af00affd0a42c517e9d75d6ec0f22
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2023-07-28 09:53:57 +0200

    [RW610] Cleaning PlatformManagerImpl.cpp from unused code

--- src/platform/nxp/rt/rw610/PlatformManagerImpl.cpp
+++ src/platform/nxp/rt/rw610/PlatformManagerImpl.cpp
@@ -49,55 +49,6 @@
 #include "fwk_platform_ot.h"
 #endif
 
-#if !CHIP_DEVICE_CONFIG_ENABLE_THREAD && !CHIP_DEVICE_CONFIG_ENABLE_WPA
-
-#include "board.h"
-#include "pin_mux.h"
-#include "clock_config.h"
-#include "fsl_silicon_id.h"
-#include "fsl_phy.h"
-
-#include "fsl_phyksz8081.h"
-#include "fsl_enet_mdio.h"
-
-#include "lwip/opt.h"
-#include "lwip/netifapi.h"
-#include "lwip/netif.h"
-#include "lwip/tcpip.h"
-#include "netif/ethernet.h"
-#include "enet_ethernetif.h"
-#include "fsl_debug_console.h"
-
-//----LWIP defines----//
-
-/* Address of PHY interface. */
-#define EXAMPLE_PHY_ADDRESS BOARD_ENET0_PHY_ADDRESS
-
-/* MDIO operations. */
-#define EXAMPLE_MDIO_OPS enet_ops
-
-/* PHY operations. */
-#define EXAMPLE_PHY_OPS phyksz8081_ops
-
-/* ENET clock frequency. */
-#define EXAMPLE_CLOCK_FREQ CLOCK_GetFreq(kCLOCK_IpgClk)
-
-#ifndef EXAMPLE_NETIF_INIT_FN
-/*! @brief Network interface initialization function. */
-#define EXAMPLE_NETIF_INIT_FN ethernetif0_init
-#endif /* EXAMPLE_NETIF_INIT_FN */
-
-/*! @brief Stack size of the temporary lwIP initialization thread. */
-#define INIT_THREAD_STACKSIZE 1024
-
-/*! @brief Priority of the temporary lwIP initialization thread. */
-#define INIT_THREAD_PRIO DEFAULT_THREAD_PRIO
-
-static mdio_handle_t mdioHandle = {.ops = &EXAMPLE_MDIO_OPS};
-static phy_handle_t phyHandle   = {.phyAddr = EXAMPLE_PHY_ADDRESS, .mdioHandle = &mdioHandle, .ops = &EXAMPLE_PHY_OPS};
-
-#endif
-
 extern "C" void BOARD_InitHardware(void);
 
 #if CHIP_DEVICE_CONFIG_ENABLE_WPA
@@ -122,55 +73,6 @@ extern "C" void vApplicationMallocFailedHook(void)
 {
     ChipLogError(DeviceLayer, "Malloc Failure");
 }
-
-#if WIFI_PTA_ENABLED && (CHIP_DEVICE_CONFIG_ENABLE_WPA && CHIP_ENABLE_OPENTHREAD)
-#ifdef SD8801
-#define HOSTCMD_RESP_BUFF_SIZE 1024
-/* 8801 command buffer to enable External Coexistence.
- * Source: SDK 2.11.1, wifi_cli demo, wlan_tests.c file. */
-const uint8_t ext_coex_8801_cmd_buf[] = {0xe0, 0,    0x1d, 0, 0x17, 0,    0,    0,    0x01, 0,    0,    0,    0x2f, 0x02, 0x0d,
-                                         0x00, 0x01, 0,    0, 0x03, 0x01, 0x02, 0x01, 0x01, 0x00, 0x28, 0x00, 0x3c, 0x00};
-#endif
-
-static CHIP_ERROR EnableWiFiCoexistence(void)
-{
-    CHIP_ERROR ret = CHIP_NO_ERROR;
-
-#ifdef SD8801
-    int status        = WM_FAIL;
-    uint8_t *rspBuff  = NULL;
-    uint32_t reqd_len = 0;
-
-    rspBuff = (uint8_t *)pvPortMalloc(HOSTCMD_RESP_BUFF_SIZE);
-    if (rspBuff != NULL)
-    {
-        status = wlan_send_hostcmd((void *)ext_coex_8801_cmd_buf, sizeof(ext_coex_8801_cmd_buf) / sizeof(u8_t), rspBuff,
-                                HOSTCMD_RESP_BUFF_SIZE, &reqd_len);
-
-        if (status == WM_SUCCESS)
-        {
-            ChipLogProgress(DeviceLayer, "8801 Coexistence enabled");
-        }
-        else
-        {
-            ChipLogError(DeviceLayer, "8801 Coexistence enabling failed = %d", status);
-            ret = CHIP_ERROR_INTERNAL;
-        }
-
-        vPortFree(rspBuff);
-    }
-    else
-    {
-        ChipLogError(DeviceLayer, "Failed to allocate memory for Wi-Fi coexistence response buffer");
-    }
-#else
-    ChipLogError(DeviceLayer, "This chip does not support Wi-Fi and OpenThread coexistence");
-    ret = CHIP_ERROR_INTERNAL;
-#endif
-
-    return ret;
-}
-#endif
 
 #if !CHIP_DEVICE_CONFIG_ENABLE_WPA
 extern "C" void vApplicationIdleHook(void)
@@ -230,16 +132,6 @@ CHIP_ERROR PlatformManagerImpl::WiFiInterfaceInit(void)
 
     ChipLogProgress(DeviceLayer, "Initialize WLAN");
 
-#if defined(SD8801) && CHIP_ENABLE_OPENTHREAD
-    /*
-     * The 88W8801 is quirky with the REQ & PRIO lines
-     * at boot: they need to be held HIGH while booting.
-     * So disable the coexistence mechanism on the RCP,
-     * since internally this puts the lines HIGH.
-     */
-    otPlatRadioSetCoexEnabled(NULL, false);
-#endif
-
     wifi_status = wlan_init((uint8_t*)WIFI_FW_BIN_ADDRESS, wlan_fw_bin_len);
     if (wifi_status != WM_SUCCESS)
     {
@@ -251,74 +143,7 @@ CHIP_ERROR PlatformManagerImpl::WiFiInterfaceInit(void)
         ChipLogProgress(DeviceLayer, "WLAN initialized");
     }
 
-#if defined(SD8801) && CHIP_ENABLE_OPENTHREAD
-    /* All done, so re-enable the coexistence mechanism on the RCP */
-    otPlatRadioSetCoexEnabled(NULL, true);
-#endif
-
-#if WIFI_PTA_ENABLED && (CHIP_ENABLE_OPENTHREAD)
-    if (result == CHIP_NO_ERROR)
-    {
-        /* In case we have both Wi-Fi and OpenThread active, enable coexistence on Wi-Fi side */
-        if (EnableWiFiCoexistence() != CHIP_NO_ERROR)
-        {
-            ChipLogError(DeviceLayer, "Failed to initialize Wi-Fi and OpenThread coexistence");
-        }
-    }
-#endif
-
     return result;
-}
-#endif
-
-#if !CHIP_DEVICE_CONFIG_ENABLE_THREAD && !CHIP_DEVICE_CONFIG_ENABLE_WPA
-CHIP_ERROR PlatformManagerImpl::EthernetInterfaceInit()
-{
-             
-    static struct netif netif;
-    ethernetif_config_t enet_config = {
-        	.phyHandle  = &phyHandle,
-#ifdef configMAC_ADDR
-        	.macAddress = configMAC_ADDR,
-#endif
-    };
-    	
-    mdioHandle.resource.csrClock_Hz = EXAMPLE_CLOCK_FREQ;
-
-#ifndef configMAC_ADDR
-    /* Set special address for each chip. */
-    (void)SILICONID_ConvertToMacAddr(&enet_config.macAddress);
-#endif
-
-    netifapi_netif_add(&netif, &enet_config, EXAMPLE_NETIF_INIT_FN,
-                       tcpip_input);
-
-    netifapi_netif_set_default(&netif);
-    netifapi_netif_set_up(netif_default);
-    LOCK_TCPIP_CORE();
-    netif_create_ip6_linklocal_address(netif_default, 1);
-    UNLOCK_TCPIP_CORE(); 
-    
-    vTaskDelay(pdMS_TO_TICKS(1500));
-    
-    for (uint8_t i = 0; i < LWIP_IPV6_NUM_ADDRESSES; i++)
-    {
-        const char *str_ip = "-";
-        if (ip6_addr_isvalid(netif_ip6_addr_state(netif_default, i)))
-        {
-            str_ip = ip6addr_ntoa(netif_ip6_addr(netif_default, i));
-        }
-        PRINTF(" IPv6 Address%d    : %s\r\n", i, str_ip);
-    }
-    PRINTF("************************************************\r\n");
-
-    
-    if(netif_is_up(&netif))
-    {
-      return CHIP_NO_ERROR;
-    }
-    
-    return CHIP_ERROR_NOT_CONNECTED;
 }
 #endif
 
@@ -404,15 +229,6 @@ CHIP_ERROR PlatformManagerImpl::_InitChipStack(void)
     err = ServiceInit();
     SuccessOrExit(err);
 
-#elif !CHIP_DEVICE_CONFIG_ENABLE_THREAD
-    err = EthernetInterfaceInit();
-
-    if (err != CHIP_NO_ERROR)
-    {
-        ChipLogProgress(DeviceLayer, "LWIP interface initialization failed. Make sure there is internet conectivity on the board and reset!");
-        chipDie();
-    }
-    ChipLogProgress(DeviceLayer, "LWIP interface initialization done");
 #endif
 
     // Call _InitChipStack() on the generic implementation base class
