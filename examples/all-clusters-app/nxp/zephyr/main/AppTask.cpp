057b7d4bd09ee51c65c7e7600776c67b5e89f07d
Axel Le Bourhis <axel.lebourhis@nxp.com>
2023-10-04 12:06:16 +0200

    [Zephyr examples] Use the AES128 key from Kconfig to decrypt factory data
    With this update, if CONFIG_CHIP_ENCRYPTED_FACTORY_DATA is set, the AES128
    used by the application will be retrieved from CONFIG_CHIP_ENCRYPTED_FACTORY_DATA_AES128_KEY if set
    or from the hardcoded value from the code.
    This allows to generate simply an encrypted factory_data and use the correct key in the application.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

--- examples/all-clusters-app/nxp/zephyr/main/AppTask.cpp
+++ examples/all-clusters-app/nxp/zephyr/main/AppTask.cpp
@@ -76,21 +76,35 @@ constexpr EndpointId kNetworkCommissioningEndpointSecondary = 0xFFFE;
 uint8_t sTestEventTriggerEnableKey[TestEventTriggerDelegate::kEnableKeyLength] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
                                                                                    0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
 
-#if CONFIG_CHIP_FACTORY_DATA
+#if CONFIG_CHIP_FACTORY_DATA && CONFIG_CHIP_ENCRYPTED_FACTORY_DATA
+
+#ifdef CONFIG_CHIP_ENCRYPTED_FACTORY_DATA_AES128_KEY
+
+#define KEY CONFIG_CHIP_ENCRYPTED_FACTORY_DATA_AES128_KEY
+#define HEXTONIBBLE(c) (*(c) >= 'A' ? (*(c) - 'A')+10 : (*(c)-'0'))
+#define HEXTOBYTE(c) (HEXTONIBBLE(c)*16 + HEXTONIBBLE(c+1))
+
+#define AES128_KEY_ARRAY    HEXTOBYTE(KEY+0),  HEXTOBYTE(KEY+2),  HEXTOBYTE(KEY+4),  HEXTOBYTE(KEY+6),  \
+                            HEXTOBYTE(KEY+8),  HEXTOBYTE(KEY+10), HEXTOBYTE(KEY+12), HEXTOBYTE(KEY+14), \
+                            HEXTOBYTE(KEY+16), HEXTOBYTE(KEY+18), HEXTOBYTE(KEY+20), HEXTOBYTE(KEY+22), \
+                            HEXTOBYTE(KEY+24), HEXTOBYTE(KEY+26), HEXTOBYTE(KEY+28), HEXTOBYTE(KEY+30)
+#else
+
+#define AES128_KEY_ARRAY    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, \
+                            0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+#endif /* CONFIG_CHIP_ENCRYPTED_FACTORY_DATA_AES128_KEY */
+
 /*
 * Test key used to encrypt factory data before storing it to the flash.
 */
-static const uint8_t aes128TestKey[] __attribute__((aligned)) = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
-                                                                0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-#endif /* CONFIG_CHIP_FACTORY_DATA */
+static const uint8_t aes128TestKey[] __attribute__((aligned)) = { AES128_KEY_ARRAY };
+
+#endif /* CONFIG_CHIP_FACTORY_DATA && CONFIG_CHIP_ENCRYPTED_FACTORY_DATA */
 
 K_MSGQ_DEFINE(sAppEventQueue, sizeof(AppEvent), kAppEventQueueSize, alignof(AppEvent));
 k_timer sFunctionTimer;
 
 chip::DeviceLayer::DeviceInfoProviderImpl gExampleDeviceInfoProvider;
-
-Identify sIdentify = { kIdentifyEndpointId, AppTask::IdentifyStartHandler, AppTask::IdentifyStopHandler,
-                       Clusters::Identify::IdentifyTypeEnum::kVisibleIndicator };
 
 bool sIsNetworkProvisioned = false;
 bool sIsNetworkEnabled     = false;
@@ -152,8 +166,10 @@ CHIP_ERROR AppTask::Init()
     k_timer_user_data_set(&sFunctionTimer, this);
 
 #if CONFIG_CHIP_FACTORY_DATA
+#if CONFIG_CHIP_ENCRYPTED_FACTORY_DATA
     FactoryDataPrvdImpl().SetEncryptionMode(FactoryDataProvider::encrypt_ecb);
     FactoryDataPrvdImpl().SetAes128Key(&aes128TestKey[0]);
+#endif /* CONFIG_CHIP_ENCRYPTED_FACTORY_DATA */
     ReturnErrorOnFailure(FactoryDataPrvdImpl().Init());
     if (err == CHIP_NO_ERROR)
     {
@@ -164,7 +180,7 @@ CHIP_ERROR AppTask::Init()
 #else
     SetDeviceInstanceInfoProvider(&DeviceInstanceInfoProviderMgrImpl());
     SetDeviceAttestationCredentialsProvider(Examples::GetExampleDACProvider());
-#endif
+#endif /* CONFIG_CHIP_FACTORY_DATA */
 
     // Initialize CHIP server
     static CommonCaseDeviceServerInitParams initParams;
