3c91260e84d7b98b1a9c35e16fcc02293c055209
Gatien Chapon <gatien.chapon@nxp.com>
2023-12-19 17:46:10 +0100

    [RT][COMMON] NXP SDK hook - updating correct ffconfig.h license
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- third_party/nxp/rt_sdk/sdk_hook/fatfs/config/ffconf.h
+++ third_party/nxp/rt_sdk/sdk_hook/fatfs/config/ffconf.h
@@ -1,9 +1,24 @@
-/*
- *  Copyright 2023 NXP
- *  All rights reserved.
- *
- *  SPDX-License-Identifier: BSD-3-Clause
- */
+/*----------------------------------------------------------------------------/
+/  FatFs - Generic FAT Filesystem Module  R0.14b                              /
+/-----------------------------------------------------------------------------/
+/
+/ Copyright (C) 2021, ChaN, all right reserved.
+/ Copyright 2023 NXP
+/
+/
+/ FatFs module is an open source software. Redistribution and use of FatFs in
+/ source and binary forms, with or without modification, are permitted provided
+/ that the following condition is met:
+/
+/ 1. Redistributions of source code must retain the above copyright notice,
+/    this condition and the following disclaimer.
+/
+/ This software is provided by the copyright holder and contributors "AS IS"
+/ and any warranties related to this software are DISCLAIMED.
+/ The copyright owner or contributors be NOT LIABLE for any damages caused
+/ by use of this software.
+/
+/----------------------------------------------------------------------------*/
 
 #ifndef _FFCONF_H_
 #define _FFCONF_H_
