773d7d47b29b46ff21a1c554bc826a1594156681
Gatien Chapon <gatien.chapon@nxp.com>
2024-01-12 11:45:41 +0100

    [RT] New west config to support SDK 2.15 type github architecture
    + IW612 2E M2 module support
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- third_party/nxp/rt_sdk/transceiver/app_transceiver_config.h
+++ third_party/nxp/rt_sdk/transceiver/app_transceiver_config.h
@@ -10,7 +10,8 @@
 
 #if defined(WIFI_IW416_BOARD_AW_AM457_USD) || defined(WIFI_IW416_BOARD_AW_AM510_USD) ||                                            \
     defined(WIFI_88W8987_BOARD_AW_CM358_USD) || defined(WIFI_IW612_BOARD_RD_USD) || defined(WIFI_BOARD_RW610) ||                   \
-    defined(WIFI_88W8801_BOARD_MURATA_2DS_USD) || defined(WIFI_IW612_BOARD_MURATA_2EL_USD)
+    defined(WIFI_88W8801_BOARD_MURATA_2DS_USD) || defined(WIFI_IW612_BOARD_MURATA_2EL_USD) ||                                      \
+    defined(WIFI_IW612_BOARD_MURATA_2EL_M2)
 #define WIFI_TRANSCEIVER_SUPPORT 1
 #else
 #define WIFI_TRANSCEIVER_SUPPORT 0
