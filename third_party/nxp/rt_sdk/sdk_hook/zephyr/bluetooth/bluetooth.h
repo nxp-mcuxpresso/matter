4afc143b186f8b9ac3164905261420098e83ea3a
Gatien Chapon <gatien.chapon@nxp.com>
2024-02-21 18:21:01 +0100

    [nxp noup][rt_sdk][sdk hook] Removing __ZEPHYR__ flag from bt_ble.gni file to avoid SDK conflict with Zephyr files
    + updating SDK hook to avoid to define __ZEPHYR__ when including SDK files
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- third_party/nxp/rt_sdk/sdk_hook/zephyr/bluetooth/bluetooth.h
+++ third_party/nxp/rt_sdk/sdk_hook/zephyr/bluetooth/bluetooth.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 NXP
+ * Copyright 2023-2024 NXP
  * All rights reserved.
  *
  *
@@ -10,7 +10,27 @@
 extern "C" {
 #endif /* _cplusplus */
 
+/*
+ * Hook file that is used to allow to build Zephyr/BLEManagerImpl.cpp with NXP SDK on a freeRTOS OS
+ */
+
+/*
+ * __ZEPHYR__ flag is mandatory to build Zephyr/BLEManagerImpl.cpp file to enable MapErrorZephyr function
+ * Nevertheless this flag should not be used in NXP SDK ".h" file when building a freeRTOS OS, therefore undef it when a NXP SDK
+ * file is included.
+ */
+#ifdef __ZEPHYR__
+#define ZEPHYR_FLAG_DEFINED 1
+#undef __ZEPHYR__
+#else
+#define ZEPHYR_FLAG_DEFINED 0
+#endif
+
 #include <bluetooth/bluetooth.h>
+
+#if ZEPHYR_FLAG_DEFINED
+#define __ZEPHYR__
+#endif
 
 #if defined(__cplusplus)
 }
