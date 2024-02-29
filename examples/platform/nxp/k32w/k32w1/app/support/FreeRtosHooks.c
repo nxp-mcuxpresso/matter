f9afeeb5e1034877572f6a4e6813424fbded1631
Marian Chereji <marian.chereji@nxp.com>
2024-03-01 01:32:36 +0200

    [nxp noup][platform][k32w1] Add "chip_key_storage" solution selector
    Updated the K32W1 platform build files by adding the "chip_key_storage"
    selector which will help developers select an implementation option
    for the key storage between "littlefs" and "fwk_nvm".
    
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>

--- examples/platform/nxp/k32w/k32w1/app/support/FreeRtosHooks.c
+++ examples/platform/nxp/k32w/k32w1/app/support/FreeRtosHooks.c
@@ -27,7 +27,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#if (CHIP_PLAT_NVM_SUPPORT == 1)
 #include "NVM_Interface.h"
+#endif
 #include "PWR_Interface.h"
 #include "board.h"
 #include "fsl_os_abstraction.h"
@@ -139,7 +141,7 @@ void vApplicationIdleHook(void)
     // to ensure there is no context switch during the actual
     // writing, thus avoiding race conditions.
     OSA_InterruptDisable();
-#if CHIP_PLAT_NVM_SUPPORT
+#if (CHIP_PLAT_NVM_SUPPORT == 1)
     NvIdle();
 #endif
     OSA_InterruptEnable();
