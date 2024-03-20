44e0f3ba06fadb524c9c24a73102c290c7bb6482
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2024-03-20 18:00:07 +0100

    Revert "[nxp noup][examples][mw320] Adding examples updates"
    This reverts commit ce40fa2595a203397fe7ec89d940bac411bc8d37.

--- examples/all-clusters-app/nxp/mw320/binding-handler.cpp
+++ examples/all-clusters-app/nxp/mw320/binding-handler.cpp
@@ -25,8 +25,6 @@
 #include "controller/InvokeInteraction.h"
 #include "lib/core/CHIPError.h"
 #include "platform/CHIPDeviceLayer.h"
-#include "AppTask.h"
-
 
 #if defined(ENABLE_CHIP_SHELL)
 #include "lib/shell/Engine.h"
@@ -70,42 +68,34 @@ static void RegisterSwitchCommands()
 }
 #endif // defined(ENABLE_CHIP_SHELL)
 
-bool bnd_notify_done=true;
-#define BINDMGMT_MAX_CMD_TIMEOUT    30
 static void BoundDeviceChangedHandler(const EmberBindingTableEntry & binding, chip::OperationalDeviceProxy * peer_device,
                                       void * context)
 {
     using namespace chip;
     using namespace chip::app;
 
-    if (binding.type == EMBER_MULTICAST_BINDING)
+    if (binding.type == MATTER_MULTICAST_BINDING)
     {
         ChipLogError(NotSpecified, "Group binding is not supported now");
         return;
     }
 
-    if (binding.type == EMBER_UNICAST_BINDING &&
+    if (binding.type == MATTER_UNICAST_BINDING && binding.local == 1 &&
         (!binding.clusterId.HasValue() || binding.clusterId.Value() == Clusters::OnOff::Id))
     {
         auto onSuccess = [](const ConcreteCommandPath & commandPath, const StatusIB & status, const auto & dataResponse) {
             ChipLogProgress(NotSpecified, "OnOff command succeeds");
-            bnd_notify_done = true;
         };
         auto onFailure = [](CHIP_ERROR error) {
             ChipLogError(NotSpecified, "OnOff command failed: %" CHIP_ERROR_FORMAT, error.Format());
-            bnd_notify_done = true;
         };
-        const Optional<uint16_t> tmoutInvokeCmdMs=chip::Optional<uint16_t>(20000);
-        const Optional<System::Clock::Timeout> tmoutResp = Optional<System::Clock::Timeout>::Value(15000);
         // Note: Need to change using toggle command since sSwitchOnOffState won't be changed if not triggerred from the switch
         // command (SwitchCommandHandler)
         {
             Clusters::OnOff::Commands::Toggle::Type toggleCommand;
             VerifyOrDie(peer_device != nullptr && peer_device->ConnectionReady());
             Controller::InvokeCommandRequest(peer_device->GetExchangeManager(), peer_device->GetSecureSession().Value(),
-                                             binding.remote, toggleCommand, onSuccess, onFailure
-                                             , tmoutInvokeCmdMs, tmoutResp
-                                             );
+                                             binding.remote, toggleCommand, onSuccess, onFailure);
         }
     }
 }
