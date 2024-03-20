b64f0a577ac095a14600f006e63ff3267a6b99c1
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2024-03-20 18:00:13 +0100

    Revert "[nxp noup][examples][rw61x] Adding examples updates"
    This reverts commit f262b81ac293e21b24ee770baee7dd90856df9c7.

--- examples/all-clusters-app/nxp/rt/rw61x/include/config/CHIPProjectConfig.h
+++ examples/all-clusters-app/nxp/rt/rw61x/include/config/CHIPProjectConfig.h
@@ -50,13 +50,8 @@
 
 #if !CONFIG_CHIP_PLAT_LOAD_REAL_FACTORY_DATA
 // Use a default pairing code if one hasn't been provisioned in flash.
-#ifndef CHIP_DEVICE_CONFIG_USE_TEST_SETUP_PIN_CODE
 #define CHIP_DEVICE_CONFIG_USE_TEST_SETUP_PIN_CODE 20202021
-#endif
-
-#ifndef CHIP_DEVICE_CONFIG_USE_TEST_SETUP_DISCRIMINATOR
 #define CHIP_DEVICE_CONFIG_USE_TEST_SETUP_DISCRIMINATOR 0xF00
-#endif
 
 // Use a default pairing code if one hasn't been provisioned in flash.
 #define CHIP_DEVICE_CONFIG_USE_TEST_PAIRING_CODE "CHIPUS"
