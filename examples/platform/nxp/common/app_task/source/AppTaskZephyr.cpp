344fc5dfa8df8157a1aafaf24b7d7beb9962a02b
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2024-03-20 17:58:46 +0100

    Revert "[nxp noup][examples][zephyr] Use common Zephyr wifi driver"
    This reverts commit 8b243eb152d55c3cc1c2ce83a72995c7c1dcd51c.

--- examples/platform/nxp/common/app_task/source/AppTaskZephyr.cpp
+++ examples/platform/nxp/common/app_task/source/AppTaskZephyr.cpp
@@ -29,7 +29,7 @@
 #include <zephyr/logging/log.h>
 
 #ifdef CONFIG_CHIP_WIFI
-#include <platform/Zephyr/wifi/ZephyrWifiDriver.h>
+#include <platform/nxp/zephyr/wifi/NxpWifiDriver.h>
 #endif
 
 #if CONFIG_CHIP_FACTORY_DATA
@@ -62,8 +62,7 @@ K_MSGQ_DEFINE(sAppEventQueue, sizeof(AppEvent), kAppEventQueueSize, alignof(AppE
 #if defined(CONFIG_CHIP_WIFI)
 chip::DeviceLayer::NetworkCommissioning::WiFiDriver * chip::NXP::App::AppTaskZephyr::GetWifiDriverInstance()
 {
-    return static_cast<chip::DeviceLayer::NetworkCommissioning::WiFiDriver *>(
-        &(NetworkCommissioning::ZephyrWifiDriver::Instance()));
+    return static_cast<chip::DeviceLayer::NetworkCommissioning::WiFiDriver *>(&(NetworkCommissioning::NxpWifiDriver::Instance()));
 }
 #endif // CONFIG_CHIP_WIFI
 
