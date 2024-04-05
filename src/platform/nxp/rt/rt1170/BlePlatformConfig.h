57a98bf52c77efa2d11acbd174d7020b225718c2
Gatien Chapon <gatien.chapon@nxp.com>
2024-04-05 09:59:39 +0200

    [nxp noup][platform][rt1170] Updating NXP copyright
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- src/platform/nxp/rt/rt1170/BlePlatformConfig.h
+++ src/platform/nxp/rt/rt1170/BlePlatformConfig.h
@@ -1,6 +1,7 @@
 /*
  *
  *    Copyright (c) 2021 Project CHIP Authors
+ *    Copyright 2023 NXP
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
