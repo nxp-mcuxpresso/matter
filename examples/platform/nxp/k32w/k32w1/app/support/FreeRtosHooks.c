16742c65647d0b758e1d555845e31e2991286020
Andrei Menzopol <andrei.menzopol@nxp.com>
2024-04-10 08:14:05 -0700

    Revert "[nxp noup][platform][k32w1] Add "chip_key_storage" solution selector"
    This reverts commit f9afeeb5e1034877572f6a4e6813424fbded1631.
    
    Signed-off-by: Andrei Menzopol <andrei.menzopol@nxp.com>

--- examples/platform/nxp/k32w/k32w1/app/support/FreeRtosHooks.c
+++ examples/platform/nxp/k32w/k32w1/app/support/FreeRtosHooks.c
@@ -27,9 +27,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#if (CHIP_PLAT_NVM_SUPPORT == 1)
 #include "NVM_Interface.h"
-#endif
 #include "PWR_Interface.h"
 #include "board.h"
 #include "fsl_os_abstraction.h"
@@ -141,7 +139,7 @@ void vApplicationIdleHook(void)
     // to ensure there is no context switch during the actual
     // writing, thus avoiding race conditions.
     OSA_InterruptDisable();
-#if (CHIP_PLAT_NVM_SUPPORT == 1)
+#if CHIP_PLAT_NVM_SUPPORT
     NvIdle();
 #endif
     OSA_InterruptEnable();
