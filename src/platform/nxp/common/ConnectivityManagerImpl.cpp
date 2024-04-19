8ff16d35755c8cd3c4746056cdcd67daf9a19581
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2024-04-19 10:48:12 +0200

    [nxp noup][platform][common] Support building matter lib and port files with cmake on nxp freertos platforms
    Signed-off-by: Dina Benamar <dina.benamarelmaaroufi@nxp.com>
    (cherry picked from commit da2b6f83af745a336bb38f181d85e9c202b635ba)

--- src/platform/nxp/common/ConnectivityManagerImpl.cpp
+++ src/platform/nxp/common/ConnectivityManagerImpl.cpp
@@ -322,7 +322,6 @@ int ConnectivityManagerImpl::_WlanEventCallback(enum wlan_event_reason wlanEvent
 
 void ConnectivityManagerImpl::OnStationConnected()
 {
-    CHIP_ERROR err;
     ChipDeviceEvent event;
 
     event.Type                          = DeviceEventType::kWiFiConnectivityChange;
@@ -335,7 +334,6 @@ void ConnectivityManagerImpl::OnStationConnected()
 
 void ConnectivityManagerImpl::OnStationDisconnected()
 {
-    CHIP_ERROR err;
     ChipDeviceEvent event;
 
     event.Type                          = DeviceEventType::kWiFiConnectivityChange;
@@ -467,7 +465,6 @@ void ConnectivityManagerImpl::_NetifExtCallback(struct netif * netif, netif_nsc_
 void ConnectivityManagerImpl::StartWiFiManagement()
 {
     struct netif * netif = nullptr;
-    EventBits_t bits;
     int32_t result;
 
     LOCK_TCPIP_CORE();
@@ -607,7 +604,6 @@ CHIP_ERROR ConnectivityManagerImpl::ProvisionWiFiNetwork(const char * ssid, uint
 #if CHIP_DEVICE_CONFIG_ENABLE_WPA
     CHIP_ERROR ret                     = CHIP_NO_ERROR;
     struct wlan_network * pNetworkData = (struct wlan_network *) malloc(sizeof(struct wlan_network));
-    int result;
 
     VerifyOrExit(pNetworkData != NULL, ret = CHIP_ERROR_NO_MEMORY);
     VerifyOrExit(ssidLen <= IEEEtypes_SSID_SIZE, ret = CHIP_ERROR_INVALID_ARGUMENT);
