fb09b9c32975fc92d048e866f7ac6ac1216313a6
marius-alex-tache <marius.tache@nxp.com>
2024-08-05 14:41:29 +0300

    Revert "[nxp noup][common][k32w0] Update some files after k32w parent removal"
    This reverts commit 7b35de86c002d97021bc1d770e056dc304fb1431.

--- scripts/build/builders/nxp.py
+++ scripts/build/builders/nxp.py
@@ -57,7 +57,7 @@ class NxpBoard(Enum):
 
     def FolderName(self, os_env):
         if self == NxpBoard.K32W0:
-            return 'k32w0'
+            return 'k32w/k32w0'
         elif self == NxpBoard.K32W1:
             return 'k32w1'
         elif self == NxpBoard.RW61X:
