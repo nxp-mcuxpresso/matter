1bb451c419b11f30733c3070cfa6c50e4876ca14
Martin Girardot <martin.girardot@nxp.com>
2024-08-08 09:53:38 +0200

    Revert "[nxp noup][zephyr] Updating NXP Copyright"
    This reverts commit 4ea299b24065cf83d4c246dc5f845b30d5036b18.
    
    Signed-off-by: Martin Girardot <martin.girardot@nxp.com>

--- src/platform/nxp/zephyr/Ethernet/NxpEthDriver.h
+++ src/platform/nxp/zephyr/Ethernet/NxpEthDriver.h
@@ -1,7 +1,6 @@
 /*
  *
  *    Copyright (c) 2024 Project CHIP Authors
- *    Copyright 2024 NXP
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
