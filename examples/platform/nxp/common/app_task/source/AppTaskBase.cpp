55e7f042687c3b388201bab447c4e539e7a60e02
marius-alex-tache <marius.tache@nxp.com>
2024-08-12 20:17:42 +0300

    Revert "[nxp noup][examples][common] Add additional common components"
    This reverts commit 267b0dcfa15f64c14e8afff32e456524dcb48f41.

--- examples/platform/nxp/common/app_task/source/AppTaskBase.cpp
+++ examples/platform/nxp/common/app_task/source/AppTaskBase.cpp
@@ -22,6 +22,7 @@
 #include "AppFactoryData.h"
 #include "CHIPDeviceManager.h"
 #include "CommonDeviceCallbacks.h"
+#include "OperationalKeystore.h"
 
 #include <app/server/Dnssd.h>
 #include <lib/dnssd/Advertiser.h>
@@ -49,10 +50,6 @@
 
 #if CONFIG_CHIP_APP_WIFI_CONNECT_AT_BOOT
 #include "WifiConnect.h"
-#endif
-
-#if CONFIG_OPERATIONAL_KEYSTORE
-#include "OperationalKeystore.h"
 #endif
 
 #if CONFIG_CHIP_OTA_PROVIDER
@@ -138,7 +135,7 @@ void chip::NXP::App::AppTaskBase::InitServer(intptr_t arg)
     initParams.testEventTriggerDelegate = &sTestEventTriggerDelegate;
 #endif
 
-#if CONFIG_OPERATIONAL_KEYSTORE
+#if CHIP_CRYPTO_PLATFORM
     initParams.operationalKeystore = chip::NXP::App::OperationalKeystore::GetInstance();
 #endif
     (void) initParams.InitializeStaticResourcesBeforeServerInit();
@@ -154,7 +151,7 @@ void chip::NXP::App::AppTaskBase::InitServer(intptr_t arg)
 
     VerifyOrDie((chip::Server::GetInstance().Init(initParams)) == CHIP_NO_ERROR);
     auto * persistentStorage = &Server::GetInstance().GetPersistentStorage();
-#if CONFIG_OPERATIONAL_KEYSTORE
+#if CHIP_CRYPTO_PLATFORM
     chip::NXP::App::OperationalKeystore::Init(persistentStorage);
 #endif
     gExampleDeviceInfoProvider.SetStorageDelegate(persistentStorage);
