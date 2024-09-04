9ac545ed765f1a10da19df315ecfd820eb4e1433
marius-alex-tache <marius.tache@nxp.com>
2024-09-04 16:40:54 +0300

    Revert "[nxp noup][scripts] Fixe script issue when there is no --args in Gn command"
    This reverts commit 7a7b03d433ea274a294fdacf2c459137943b47ad.
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- scripts/build/builders/nxp.py
+++ scripts/build/builders/nxp.py
@@ -255,8 +255,7 @@ class NxpBuilder(GnBuilder):
                         cmd += 'export NXP_K32W0_SDK_ROOT="' + str(p.sdk_storage_location_abspath) + '" \n '
                     elif p.sdk_name == 'common':
                         cmd += 'export NXP_SDK_ROOT="' + str(p.sdk_storage_location_abspath) + '" \n '
-            # add empty space at the end to avoid concatenation issue when there is no --args
-            cmd += 'gn gen --check --fail-on-unused-args --export-compile-commands --root=%s ' % self.root
+            cmd += 'gn gen --check --fail-on-unused-args --export-compile-commands --root=%s' % self.root
 
             extra_args = []
 
