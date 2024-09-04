3c791a745bbed05d43758b68be22ec4cd71c3f4c
marius-alex-tache <marius.tache@nxp.com>
2024-09-04 16:40:45 +0300

    Revert "Revert "[nxp noup][examples][common] Add support for TBR management cluster and secondary nwk if""
    This reverts commit 2a4f006f671f88c3e1bee8c9d6b09481db7ba647.
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- examples/platform/nxp/common/device_callbacks/include/CommonDeviceCallbacks.h
+++ examples/platform/nxp/common/device_callbacks/include/CommonDeviceCallbacks.h
@@ -40,9 +40,11 @@ public:
     virtual void OnInternetConnectivityChange(const chip::DeviceLayer::ChipDeviceEvent * event);
     virtual void OnSessionEstablished(const chip::DeviceLayer::ChipDeviceEvent * event);
     virtual void OnInterfaceIpAddressChanged(const chip::DeviceLayer::ChipDeviceEvent * event);
-#if CHIP_ENABLE_OPENTHREAD && CHIP_DEVICE_CONFIG_CHIPOBLE_DISABLE_ADVERTISING_WHEN_PROVISIONED
+#if CHIP_ENABLE_OPENTHREAD
     virtual void OnComissioningComplete(const chip::DeviceLayer::ChipDeviceEvent * event);
 #endif
+private:
+    bool bIsTbrClusterInit = false;
 };
 
 class DeviceCallbacksDelegate
