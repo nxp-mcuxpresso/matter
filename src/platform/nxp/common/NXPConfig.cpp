12ae608b2d15843071bdf5d2847f28ec193469f2
marius-alex-tache <marius.tache@nxp.com>
2024-09-04 16:41:09 +0300

    Revert "[nxp noup][platform][common] Create NXPConfigNVS.cpp file, remove NXPConfig BLE dependencies"
    This reverts commit ac043551e303d772b10b7fbbb8e57e37aa7b1760.
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- src/platform/nxp/common/NXPConfig.cpp
+++ src/platform/nxp/common/NXPConfig.cpp
@@ -21,6 +21,8 @@
  *          Utilities for accessing persisted device configuration on
  *          platforms based on the  NXP SDK.
  */
+/* this file behaves like a config.h, comes first */
+#include <platform/internal/CHIPDeviceLayerInternal.h>
 
 #include "NXPConfig.h"
 
@@ -28,7 +30,6 @@
 #include "FunctionLib.h"
 #include "board.h"
 #include <lib/core/CHIPEncoding.h>
-#include <platform/CHIPDeviceError.h>
 #include <platform/internal/testing/ConfigUnitTest.h>
 
 /* FS Writes in Idle task only - LittleFS only , already enabled by default on NVM */
