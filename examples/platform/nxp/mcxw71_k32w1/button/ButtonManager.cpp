999ace55a3ff42a51c71293a87dbc875148555e3
Andrei Menzopol <andrei.menzopol@nxp.com>
2024-09-10 06:18:12 -0700

    [nxp noup][examples][mcxw71_k32w1] Multiple ble connections changes
     * Add button handle to enable multiple ble connections (start advertisement)
     * Add app_ble/BLEApplicationManager.* to handle check & start advertisement
    
    Signed-off-by: Andrei Menzopol <andrei.menzopol@nxp.com>

--- examples/platform/nxp/mcxw71_k32w1/button/ButtonManager.cpp
+++ examples/platform/nxp/mcxw71_k32w1/button/ButtonManager.cpp
@@ -20,6 +20,7 @@
 #include "AppConfig.h"
 #include "AppMatterButton.h"
 #include "AppTask.h"
+#include "BLEApplicationManager.h"
 #include "UserInterfaceFeedback.h"
 
 #include <app-common/zap-generated/attributes/Accessors.h>
@@ -185,6 +186,8 @@ void ButtonManager::BleHandler(const AppEvent & event)
 #endif
 
     chip::NXP::App::GetAppTask().SwitchCommissioningStateHandler();
+
+    chip::NXP::App::BleAppMgr().EnableMultipleConnectionsHandler();
 }
 
 void ButtonManager::CancelTimer()
