b14000dd27d4306cfd64045572486273204e27bb
Gatien Chapon <gatien.chapon@nxp.com>
2024-09-27 12:30:31 +0200

    Revert "[nxp toup][zephyr][ethernet] Add Ethernet support to nxp zephyr"
    This reverts commit 9b8698d80baccdefbc8221cea099a62a88008ef5.
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- examples/platform/nxp/common/app_task/include/AppTaskZephyr.h
+++ examples/platform/nxp/common/app_task/include/AppTaskZephyr.h
@@ -51,8 +51,6 @@ public:
      */
 #if defined(CONFIG_CHIP_WIFI)
     virtual chip::DeviceLayer::NetworkCommissioning::WiFiDriver * GetWifiDriverInstance(void) override;
-#elif defined(CONFIG_CHIP_ETHERNET)
-    virtual chip::DeviceLayer::NetworkCommissioning::EthernetDriver * GetEthernetDriverInstance(void) override;
 #endif
 
     /**
