5938d63d8907ad69b07b9b1eeff855ad72c8c89f
Gatien Chapon <gatien.chapon@nxp.com>
2024-09-27 12:30:29 +0200

    Revert "[nxp toup][platform][common] Make k32w1 BLEManager implementation common"
    This reverts commit 75620922ed817bdcad0ed92905ae8e1b00f6ebd6.
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- src/platform/nxp/common/legacy/BLEManagerCommon.h
+++ src/platform/nxp/common/legacy/BLEManagerCommon.h
@@ -20,7 +20,7 @@
 /**
  *    @file
  *          Provides an implementation of the BLEManager singleton object
- *          for NXP platforms.
+ *          for the K32W platforms.
  */
 
 #pragma once
